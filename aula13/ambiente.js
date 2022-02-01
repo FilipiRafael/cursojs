86a4e4e19b7a973641bce1c7954ef9482c9c74c4
FilipiRafael <filipirafael.123@gmail.com>
2022-02-01 09:27:09 -0300

    Finalizando exercícios

---
+++ aula13/ambiente.js
@@ -0,0 +1,34 @@
+/*
+var contador = 1
+while(contador <= 6) {
+    console.log(`Contador: ${contador}`)
+    contador++
+}
+*/
+
+var c = 1
+do {
+    console.log(`Passo: ${c}`)
+    c++
+} while (c <= 6)
+
+
+for (var c=1; c<=10; c++) {
+    
+}
+
+
+
+
+// Estrutura de repetição com teste lógico no início: while()
+// Estrutura de repetição com teste lógico no final: do while()
+// Estrutura de repetição com teste lógico (inicialização; teste lógico; incremento): for
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
